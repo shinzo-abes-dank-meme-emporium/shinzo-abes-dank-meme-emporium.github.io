370192b4f0ac31c43ad842abf6dd42e9e0d37c09
shinzo-abes-dank-meme-emporium <hong.derek.j@gmail.com>
2017-06-26 17:30:36 -0400

    ASDF

--- JAPN0400/L10/chapter-data.js
+++ JAPN0400/L10/chapter-data.js
@@ -171,31 +171,78 @@ const chap_grammar_list = [
         translation: 'If you want to visit an old city, Kyōto is the best place.'
       }
     ],
-    other: '限る means to restrict, to limit, to confine as a transitive verb.  It can also be an intransitive verb meaning to be restriected, to be limited, to be confined.'
+    other: '限る means to restrict, to limit, to confine as a transitive verb.  It can also be an intransitive verb meaning to be restricted, to be limited, to be confined.'
   },{
     grammar_point: '〜まま',
     spec: '〜まま',
-    meaning: '',
+    meaning: '"Unchanged"; "As it is"; "Still being in the state of 〜"',
     use: {
-      text: '',
+      text: '〜まま indicates that some condition/state is unchanged.  It can be attached to nouns, adjectives, and verbs.  When 〜まま modifies a noun, の follows 〜まま (ie: 〜ままの(noun)).  Can also be used with negative forms (〜ないまま).  Can also be used with demonstrative nouns (この、その、あの).  Usage of 〜まま seems to imply that there is a state that was acheived previously and that such state was/is maintained.  As such, the progressive form of verbs (〜ている) is not used since because it would be awkward (I think...)',
       species: [
         {
-          jap: '(noun)に限る',
-          eng: ''
+          jap: '(noun)のまま：',
+          eng: 'As noun is'
+        },{
+          jap: '(adjい)いまま：',
+          eng: 'As adj is (NOTE: adjい MUST be in plain, present tense)'
+        },{
+          jap: '〜ないまま：',
+          eng: 'As 〜 is not (NOTE: the negation 〜ない MUST be in plain, present tense)'
+        },{
+          jap: '(adjな)なまま：',
+          eng: 'As adj is'
+        },{
+          jap: 'verb(plain)[past]まま：',
+          eng: 'As verb is (NOTE: verb MUST be in plain, past tense.  Verb should NOT be progressive/stative 〜て form)'
         }
       ]
     },
     example: [
       {
         sentence: {
-          ante: '『暑い<b>日には</b>冷たいビールを',
-          highlight: '飲むに限る',
-          post: '。』'
+          ante: '『京都には',
+          highlight: '昔のまま',
+          post: 'のお寺や神社が多い。』'
         },
-        translation: 'Nothing is better than drinking cold beer on a hot day.'
+        translation: 'There are many old temples and shrines in Kyōto. (lit. There are many temples and shrines that are unchanged as they were in the olden days, in Kyōto.)'
+      },{
+        sentence: {
+          ante: '『日本酒（しゅ）は',
+          highlight: '冷たいまま',
+          post: '飲んでも美味しい。』'
+        },
+        translation: 'Japanese rice wine even tastes good cold. (lit. Japanese rice wine tastes good even if it is drunk being cold.)'
+      },{
+        sentence: {
+          ante: '『窓（まど）',
+          highlight: '開けたまま',
+          post: '寝ると、風邪（かぜ）をひく。』'
+        },
+        translation: 'If you go to sleep with the window open, you will catch a cold. (lit. If you go to sleep with an opened window still in the state of being so, you will catch a cold.)'
+      },{
+        sentence: {
+          ante: '『',
+          highlight: 'このまま',
+          post: 'よろしいですか。』'
+        },
+        translation: 'Is it alright like this (being unchanged)?'
+      },{
+        sentence: {
+          ante: '『彼女に別れ（わか）を',
+          highlight: '言わないまま',
+          post: '日本に来てしまった。』'
+        },
+        translation: 'I came to Japan still not having said goodbye to my girlfriend. (what is the difference between using this and using 〜ないで？）'
+      },{
+        sentence: {
+          ante: '『私の本棚（ほんだな）には',
+          highlight: '買ったまま',
+          post: '読んでないマンガがたくさんあります。』'
+        },
+        translation: 'My bookshelf has many manga that are left unread. (lit. My bookshelf has many manga that are unread, bought but unchanged.)'
       }
     ],
-    other: '限る means to restrict, to limit, to confine as a transitive verb.  It can also be an intransitive verb meaning to be restriected, to be limited, to be confined.'
+    other: 'I think the verb(plain)[past]まま construction has the literal meaning of "unchanged with respect to being verbed in the past".  As such, the verb must be past because it references something that happened in the past while まま declares that the result of what happened is yet unchanged.  As a consequence, I think the stative 〜ている form of verbs– verb(て)(plain)[past] (〜ていたまま)– is not used because まま already implies the stative aspect of a situation.  For example, in the last example sentence, 買ったまま means "unchanged since when they were bought".  The sentence makes sense without 買ったまま、but 〜まま here emphasizes that the manga are yet unread "just as when they were bought".  Additionally, when compared to verb(stem)ながら it becomes more clear that 〜まま means "to do (main)verb IN THE STATE of doing/having done (まま)verb", whereas 〜ながら means "to do (main)verb WHILE doing (ながら)verb".  But, 〜てないまま seems to be an acceptable construction.  This probably means someting like "still being in the state of not being in the state of verb" (maybe...).'
   }
 ];
 
--- JAPN0400/L11/chapter-data.js
+++ JAPN0400/L11/chapter-data.js
@@ -52,31 +52,178 @@ const chap_note_list = [
 
 const chap_grammar_list = [
   {
-    grammar_point: '無',
-    spec: '無',
-    meaning: '無',
+    grammar_point: 'verb(plain)（もん／もの）ですから',
+    spec: 'verb(plain)（もん／もの）ですから',
+    meaning: '"Because of 〜"; "〜 happened, regretably"',
     use: {
-      text: '無',
-      species: []
+      text: 'When verb is verb(plain)[past], there is a semblance of apology in the statment.  When the verb is verb(plain)[present], the speaker is just giving a reason or stating something that happened.  In both cases, the speaker feels bad for what happened and in both cases the situation could not be avoided.  The past form of verbs is used for apologies.',
+      species: [
+        {
+          jap: 'verb(plain)[past]もんですから：',
+          eng: 'Such verb happened, I appologize.'
+        },{
+          jap: 'verb(plain)[present]もんですから：',
+          eng: 'Such verb happened, regretably.'
+        }
+      ]
     },
     example: [
       {
         sentence: {
-          ante: '無',
-          highlight: '無',
+          ante: 'すみません。出かけようとしたところに家から電話が',
+          highlight: 'かかたものですから',
           post: '。』'
         },
-        translation: '無'
+        translation: 'I\'m sorry.  It\'s because my parents called me just as I was about to leave the house. (Appologetic)'
       },{
         sentence: {
-          ante: '『無',
-          highlight: '無',
+          ante: '『友達と街でばったり',
+          highlight: '出会ったもんですから',
           post: '。』'
         },
-        translation: '無'
+        translation: 'It\'s because I happened to meet my friend in the city. (Appologetic)'
+      },{
+        sentence: {
+          ante: '『このごろホストファミリーのお母さんに、よく文句',
+          highlight: '言われるもんだから',
+          post: '。』'
+        },
+        translation: '(In response to an observation that one looks down) This morning, I was scolded by my hostfamily mom. (Regretable)'
       }
     ],
-    other: '無'
+    other: '...'
+  },{
+    grammar_point: '〜うちに',
+    spec: '〜うちに',
+    meaning: '"While 〜"; "While 〜 situation holds"',
+    use: {
+      text: 'There are two versions of 〜うちに。 The second is explained below.  〜うちに is preceded by nouns, adjectives, and verbs in simple (る・／う／) or progressive/stative (〜ている) forms.  You should not use with past forms.  It expresses a sense of "while a certain situation holds".  うち means "inside", "within", and "while", among other things.',
+      species: [
+        {
+          jap: '(noun)のうちに：',
+          eng: 'While being a noun'
+        },{
+          jap: '(adjい)いうちに：',
+          eng: 'While being adjい'
+        },{
+          jap: '(adjな)なうちに：',
+          eng: 'While being adjな'
+        },{
+          jap: '(verb)(plain)うちに：',
+          eng: 'While verb is the case'
+        },{
+          jap: '(verb)(ている)(plain)うちに：',
+          eng: 'While verb state is the case'
+        }
+      ]
+    },
+    example: [
+      {
+        sentence: {
+          ante: '『',
+          highlight: '学生のうちに',
+          post: '旅行をした方がいい。』'
+        },
+        translation: 'One should travel while still a student.'
+      },{
+        sentence: {
+          ante: '『',
+          highlight: '静かな（しず）うちに',
+          post: '勉強をしておこう。』'
+        },
+        translation: 'I will get my study done while it is quiet.'
+      },{
+        sentence: {
+          ante: '『',
+          highlight: '若いうちに',
+          post: '自分のしたいことをしておくといい。』'
+        },
+        translation: 'You should do what you want while you are still young.'
+      },{
+        sentence: {
+          ante: '『日本に',
+          highlight: 'いるうちに',
+          post: '、一度富士山に登（のぼ）りたいと思います。』'
+        },
+        translation: 'I would like to climb Mt. Fuji while I am in Japan.'
+      },{
+        sentence: {
+          ante: '『',
+          highlight: '話しているうちに',
+          post: '時間がたってしまった。』'
+        },
+        translation: 'Time flew by while we were talking.'
+      }
+    ],
+    other: 'How does 〜うちに compare to 〜まま？'
+  },{
+    grammar_point: 'verb(plain)[neg.]うちに',
+    spec: 'verb(plain)[neg.]うちに',
+    meaning: '"Before 〜"; "Before 〜 happens"',
+    use: {
+      text: 'When 〜うちに is preceded by negative verbs, it means "before 〜 happens".  This only applies to verbs in the negative case.  Usually, adjectives followed by ならない will be used to convey "before something becomes adj".',
+      species: [
+        {
+          jap: '(verb)（ない・／あ／ない）うちに：',
+          eng: 'Before verb happens'
+        },{
+          jap: '(adjい／な)ならないうちに：',
+          eng: 'Before something becomes adj'
+        }
+      ]
+    },
+    example: [
+      {
+        sentence: {
+          ante: '『ラーメンが',
+          highlight: '冷たくならないうちに',
+          post: '食べなさい。』'
+        },
+        translation: 'Please eat the ramen before it becomes cold.'
+      },{
+        sentence: {
+          ante: '『母に',
+          highlight: 'しかられないうちに',
+          post: '宿題をします。』'
+        },
+        translation: 'I will do my homework before my mother scolds me.'
+      }
+    ],
+    other: '...'
+  },{
+    grammar_point: '〜が気になる',
+    spec: '〜が気になる',
+    meaning: '"〜 weighs on one\'s mind"; "To be concerned about 〜"',
+    use: {
+      text: '〜 can be a noun or a phrase ending in a nominalized verb (verbの）。',
+      species: [
+        {
+          jap: '(noun)が気になる：',
+          eng: 'To be concerned about noun'
+        },{
+          jap: '(verb)(の)が気になる',
+          eng: 'To be concerned about verb phrase'
+        }
+      ]
+    },
+    example: [
+      {
+        sentence: {
+          ante: '『',
+          highlight: '成績が気になります',
+          post: '。』'
+        },
+        translation: 'I am worried about my grade.'
+      },{
+        sentence: {
+          ante: '『ホームステイの学生の家族から手紙が一通（つう）も',
+          highlight: '来ないのが気になるます',
+          post: '。』'
+        },
+        translation: 'I am bothered by the fact that my host student\'s family has not even written a single letter to me.'
+      }
+    ],
+    other: '...'
   }
 ];
 

72db2d95534fb4606b3e8b3b85d35354a73a708f
shinzo-abes-dank-meme-emporium <hong.derek.j@gmail.com>
2017-09-08 16:16:45 -0400

    ASDF

--- js/search.js
+++ js/search.js
@@ -256,7 +256,7 @@ $(window).on('load', function() {
     }
   }
 
-  grammar_dfd.done(grammar_ajaxes).done(function () { $("#grammar-loading-text").css("display", "none"); });
+  grammar_dfd.done(grammar_ajaxes).done(function () { console.log("grammar loaded"); $("#grammar-loading-text").css("display", "none"); });
   grammar_dfd.resolve();
 
   // while(loading_grammar > 0) { $("#grammar-loading-text").css("display", "none"); }

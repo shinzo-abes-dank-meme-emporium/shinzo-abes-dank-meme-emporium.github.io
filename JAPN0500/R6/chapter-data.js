6d90666f377462662a47dcc60db7b5224bac6de6
shinzo-abes-dank-meme-emporium <hong.derek.j@gmail.com>
2017-09-11 12:40:09 -0400

    added R6

---
+++ JAPN0500/R6/chapter-data.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var local_course_name = '日本語５００';
+var local_course_path = 'JAPN0500'
+var local_chap_name = 'Ｒ６';
+var local_chap_path = 'R6'
+
+
+
+/*
+{
+  topic: '',
+  spec: '',
+  subtopic_list: [
+    {
+      subtopic: '',
+      desc: 
+      [
+        ''
+      ]
+    }
+  ]
+}
+*/
+
+var chap_note_list = [];
+
+var chap_grammar_list = [];
+
+
+/**
+{
+  grammar_point: '',
+  spec: '',
+  alt_def: '',
+    meaning: '',
+  use: {
+    text: '',
+    species: [
+      {
+        jap: '',
+        eng: ''
+      }
+    ],
+  },
+  example: [
+    {
+      sentence: {
+        ante: '',
+        highlight: '',
+        post: ''
+      },
+      translation: ''
+    },
+  ],
+  other: '...'
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JAPN0500/course-data.js
+++ JAPN0500/course-data.js
@@ -1,8 +1,11 @@
 const local_course_name = '日本語５００';
 
 const local_chap_list = [
-{
-  name: '読み物４０',
-  path: 'R40'
-}
+  {
+    name: '読み物６',
+    path: 'R6'
+  },{
+    name: '読み物４０',
+    path: 'R40'
+  }
 ];

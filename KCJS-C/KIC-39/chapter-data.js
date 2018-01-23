908d008336133483b61335727061f5489f115276
shinzo-abes-dank-meme-emporium <hong.derek.j@gmail.com>
2018-01-22 22:07:48 -0500

    kic 39

---
+++ KCJS-C/KIC-39/chapter-data.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var local_course_name = 'KCJS Class-C';
+var local_course_path = 'KCJS-C'
+var local_chap_name = 'KIC 第３９回';
+var local_chap_path = 'KIC-38'
+
+
+
+/*
+{
+  topic: '',
+  spec: '',
+  subtopic_list: [
+    {
+      subtopic: '',
+      desc: 
+      [
+        ''
+      ]
+    }
+  ]
+}
+*/
+
+var chap_note_list = [];
+
+var chap_grammar_list = [];
+
+
+/**
+{
+  grammar_point: '',
+  spec: '',
+  alt_def: '',
+    meaning: '',
+  use: {
+    text: '',
+    species: [
+      {
+        jap: '',
+        eng: ''
+      }
+    ],
+  },
+  example: [
+    {
+      sentence: {
+        ante: '',
+        highlight: '',
+        post: ''
+      },
+      translation: ''
+    },
+  ],
+  other: '...'
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+

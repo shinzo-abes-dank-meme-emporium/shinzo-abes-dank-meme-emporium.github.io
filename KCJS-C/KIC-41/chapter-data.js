e3043140638bd75c64723156f6dfbd44cad7312a
shinzo-abes-dank-meme-emporium <hong.derek.j@gmail.com>
2018-01-29 08:34:50 -0500

    ASDF

--- KCJS-C/KIC-41/chapter-data.js
+++ KCJS-C/KIC-41/chapter-data.js
@@ -2,8 +2,8 @@
 
 var local_course_name = 'KCJS Class-C';
 var local_course_path = 'KCJS-C'
-var local_chap_name = 'KIC 第４０回';
-var local_chap_path = 'KIC-40'
+var local_chap_name = 'KIC 第４１回';
+var local_chap_path = 'KIC-41'
 
 
 
